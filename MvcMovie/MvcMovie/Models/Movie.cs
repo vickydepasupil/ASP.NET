acc594b7b2a8b54c9ce32d2c1c051c8aa051e688
Victoria Depasupil <victoria.depasupil@gmail.com>
2017-08-17 14:12:53 +0800

    Initial commit

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public String Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(5)]
+        public String Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(1, 100)]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(5)]
+        public String Rating { get; set; }
+    }
+}
